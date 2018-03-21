e9a69cf7a20c74c099ba86aeef4c658d06ee6811
Adam Kollberg <adamkollberg@Adams-Air.lan>
2018-03-21 09:44:23 +0100

    progress bar done

--- scripts.js
+++ scripts.js
@@ -28,12 +28,18 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
     console.log(this.name);
   }
+  function handleProgrees(){
+      const percent = (video.currentTime / video.duration) * 100;
+      progressBar.style.flexBasis = `${percent}%`;
+  }
 
 /* Event-listners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgrees);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
